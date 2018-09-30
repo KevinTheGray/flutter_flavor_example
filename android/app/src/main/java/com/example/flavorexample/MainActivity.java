c536b9ddb1360a2e429070895068a42583fe9471
Kevin Gray <KevinTheGray8@gmail.com>
2018-09-30 18:24:48 -0400

    Added code to access flavor information

--- android/app/src/main/java/com/example/flavorexample/MainActivity.java
+++ android/app/src/main/java/com/example/flavorexample/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.flavorexample;
 
 import android.os.Bundle;
+
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
@@ -9,5 +12,11 @@ public class MainActivity extends FlutterActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+    new MethodChannel(this.getFlutterView(), "getFlavorConfiguration").setMethodCallHandler(new MethodChannel.MethodCallHandler() {
+      @Override
+      public void onMethodCall(MethodCall methodCall, MethodChannel.Result result) {
+        result.success(BuildConfig.FLAVOR_CONFIGURATION);
+      }
+    });
   }
 }
--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -4,9 +4,15 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
-    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
+  FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
+  FlutterMethodChannel * channel = [FlutterMethodChannel methodChannelWithName:@"getFlavorConfiguration"
+                                                               binaryMessenger:controller];
+  [channel setMethodCallHandler:^(FlutterMethodCall * _Nonnull call, FlutterResult  _Nonnull result) {
+    NSString *flavorConfig = (NSString *) [[NSBundle mainBundle] objectForInfoDictionaryKey:@"FlavorConfiguration"];
+    result(flavorConfig);
+  }];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
